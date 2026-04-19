f308d5a2ad544943c406068f9b8628a7359d2602
Kübra <156194000+kkubrraa@users.noreply.github.com>
2026-04-19 21:24:01 +0300

    MiniPosApp initial upload

---
+++ FrmKasa.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace MiniPosApp
+{
+    partial class FrmKasa
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataCart = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowProducts = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataCart)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataCart
+            // 
+            this.dataCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataCart.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dataCart.Location = new System.Drawing.Point(0, 0);
+            this.dataCart.Name = "dataCart";
+            this.dataCart.RowHeadersWidth = 51;
+            this.dataCart.RowTemplate.Height = 24;
+            this.dataCart.Size = new System.Drawing.Size(338, 450);
+            this.dataCart.TabIndex = 0;
+            this.dataCart.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataCart_CellContentClick);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.flowProducts, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblTotal, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(338, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(462, 450);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // flowProducts
+            // 
+            this.flowProducts.AutoScroll = true;
+            this.flowProducts.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowProducts.Location = new System.Drawing.Point(234, 3);
+            this.flowProducts.Name = "flowProducts";
+            this.flowProducts.Padding = new System.Windows.Forms.Padding(10);
+            this.flowProducts.Size = new System.Drawing.Size(225, 404);
+            this.flowProducts.TabIndex = 0;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.lblTotal, 2);
+            this.lblTotal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTotal.Location = new System.Drawing.Point(3, 410);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(456, 40);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "label1";
+            this.lblTotal.Visible = false;
+            this.lblTotal.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.dataCart);
+            this.Name = "Form1";
+            this.Text = "FrmKasa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataCart)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataCart;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowProducts;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ FrmKasa.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniPosApp
+{
+    public partial class FrmKasa : Form
+    {
+        public FrmKasa()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTotal.Visible = false;
+
+            dataCart.BorderStyle = BorderStyle.None;
+            dataCart.EnableHeadersVisualStyles= false;
+            dataCart.ColumnHeadersDefaultCellStyle.BackColor= Color.Black;
+            dataCart.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            dataCart.DefaultCellStyle.SelectionBackColor = Color.FromArgb(80,80,80);
+            dataCart.DefaultCellStyle.SelectionForeColor=Color.White;
+                
+            dataCart.RowHeadersVisible = false;
+            dataCart.AllowUserToAddRows = false;
+            dataCart.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataCart.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataCart.Columns.Clear();
+            dataCart.Columns.Add("Ad", "ürün");
+            dataCart.Columns.Add("Fiyat", "Fiyat");
+            dataCart.Columns.Add("Adet", "Adet");
+            dataCart.Columns.Add("Toplam", "Toplam");
+
+            
+            DataGridViewButtonColumn btn = new DataGridViewButtonColumn();
+            btn.DefaultCellStyle.BackColor= Color.Red;
+            btn.DefaultCellStyle.ForeColor= Color.White;
+            btn.Name = "Sil";
+            btn.HeaderText = "Sil";
+            btn.Text = "X";
+            btn.UseColumnTextForButtonValue = true;
+           
+
+            dataCart.Columns.Add(btn);
+            dataCart.Columns["Sil"].Width = 50;
+            dataCart.Columns["Adet"].Width = 60;
+
+            dataCart.Columns["Fiyat"].DefaultCellStyle.Format = "0.00";
+            dataCart.Columns["Toplam"].DefaultCellStyle.Format = "0.00";
+
+
+
+            UrunEkle("Burger", 50, "burger.jpg");
+            UrunEkle("Kola", 20, "cola.jpg");
+            UrunEkle("Pizza", 80, "pizza.jpg");
+
+
+
+
+
+        }
+
+        void UrunEkle(string ad, decimal fiyat, string resimYolu)
+        {
+           
+        
+
+           Panel p= new Panel();
+            p.BackColor = Color.White;
+            p.BorderStyle = BorderStyle.FixedSingle;
+            p.Margin = new Padding(10);
+            p.Width = 100;
+            p.Height = 130;
+            
+            PictureBox pic = new PictureBox();
+            pic.Image = Image.FromFile(resimYolu);
+            pic.SizeMode=PictureBoxSizeMode.StretchImage;
+            pic.Width = 80;
+            pic.Height = 60;
+            pic.Top = 10;
+            pic.Left = 10;
+
+
+
+            
+
+            Label lbl = new Label();
+            lbl.Text = ad;
+            lbl.Top = 10;
+            lbl.Left = 10;
+            lbl.AutoSize = true;
+
+            Label lblFiyat = new Label();
+
+            lblFiyat.Text = fiyat + "₺";
+
+            lblFiyat.Top = 35;
+
+            lblFiyat.Left = 10;
+
+            lblFiyat.AutoSize = true;
+
+            Button btn= new Button();
+            btn.Text = "Ekle";
+            btn.Width = 80;
+            btn.Height = 30;
+            btn.Top = 80;
+            btn.Left = 20;
+
+            btn.Click += (s, e) =>
+            {
+
+                SepeteEkle(ad, fiyat);
+            };
+            p.Controls.Add(pic);
+            p.Controls.Add(lbl);
+            p.Controls.Add(lblFiyat);
+            p.Controls.Add(btn);
+
+            flowProducts.Controls.Add(p);
+             }
+
+
+        void SepeteEkle(string ad, decimal fiyat)
+        {
+            bool bulundu = false;
+
+            foreach (DataGridViewRow row in dataCart.Rows)
+            {
+                if (row.Cells[0].Value != null && row.Cells[0].Value.ToString() == ad)
+                {
+                    int adet = Convert.ToInt32(row.Cells[2].Value);
+                    adet++;
+
+                    row.Cells[2].Value = adet;
+                    row.Cells[3].Value = adet * fiyat;
+
+                    bulundu = true;
+                    break;
+                }
+            }
+
+            if (!bulundu)
+            {
+                dataCart.Rows.Add(ad, fiyat, 1, fiyat);
+            }
+
+            ToplamHesapla();
+        }
+
+        void ToplamHesapla()
+        {
+            decimal toplam = 0;
+
+            foreach (DataGridViewRow row in dataCart.Rows) 
+            {
+               if (row.Cells[3].Value != null)
+                {
+                    toplam += Convert.ToDecimal(row.Cells[3].Value);
+                }
+
+                lblTotal.Text = "Toplam:" + toplam + "₺";
+                lblTotal.Visible = true;
+            }
+
+            lblTotal.Text = "Toplam: " + toplam.ToString("0.00") + "₺";
+        }
+
+        private void dataCart_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.ColumnIndex == dataCart.Columns["Sil"].Index && e.RowIndex >= 0)
+            {
+                dataCart.Rows.RemoveAt(e.RowIndex);
+                ToplamHesapla();
+            }
+        }
+    }
+}
